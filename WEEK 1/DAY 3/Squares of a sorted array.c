e44e59509ffdb35d3b462e2b58c8abe8959fff15
dusyantgupta2024-lang <dusyant.gupta2024@glbajajgroup.org>
2026-06-07 12:45:50 +0530

    Add sortedSquares function for squared values
    Implement function to return squares of sorted array

---
+++ WEEK 1/DAY 3/Squares of a sorted array.c
@@ -0,0 +1,27 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* sortedSquares(int* nums, int numsSize, int* returnSize) {
+    int* result = (int*)malloc(numsSize * sizeof(int));
+
+    int left = 0;
+    int right = numsSize - 1;
+    int index = numsSize - 1;
+
+    while (left <= right) {
+        int leftSquare = nums[left] * nums[left];
+        int rightSquare = nums[right] * nums[right];
+
+        if (leftSquare > rightSquare) {
+            result[index] = leftSquare;
+            left++;
+        } else {
+            result[index] = rightSquare;
+            right--;
+        }
+        index--;
+    }
+
+    *returnSize = numsSize;
+    return result;
+}
